b97f03459ea5a6accb610493afa4935ff4790f9e
Christoph Baier <anon@example.com>
2026-07-09 12:09:54 +0200

    release v1.2.2

--- src/server/tools/tools.go
+++ src/server/tools/tools.go
@@ -13,7 +13,7 @@ var (
 	PrettyFrontendTimeLayoutWithDay = PrettyFrontendTimeLayout + ", Mon"
 )
 
-const ApplicationVersion = "1.2.1"
+const ApplicationVersion = "1.2.2"
 
 const (
 	SampleTestRecipientEmail = "recipient@example.invalid"
